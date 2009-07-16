ea428f586451357ccde3ca7672b0ad166f68b04c
nick.dragu <nick.dragu@bd0428c2-674d-11de-9593-3d8b33745fea>
2009-07-16 20:53:49 +0000

    Beta: added rss parser, added command line functionality

---
+++ branches/beta-rss/src/application/LinkedList.java
@@ -0,0 +1,153 @@
+package application;
+
+import edu.stanford.smi.protegex.owl.model.OWLIndividual;
+
+class LinkedList 
+{
+
+    private Node firstNode;
+    private int length;
+
+    public LinkedList() 
+    {
+      clear();
+    } // end default constructor
+
+    public final void clear() 
+    {
+      firstNode = null;
+      length = 0;
+    } // end clear
+
+    public boolean add(OWLIndividual newEntry) 
+    {
+      Node newNode = new Node(newEntry);
+      if (isEmpty())
+        firstNode = newNode;
+      else 
+      {
+        Node lastNode = getNodeAt(length);
+        lastNode.next = newNode;
+      }
+      length++;
+      return true;
+    } // end add
+
+    public OWLIndividual remove(int givenPosition) 
+    {
+      OWLIndividual result = null; // return value
+
+      if (!isEmpty() && (givenPosition >= 1) && (givenPosition <= length)) 
+      {
+        assert !isEmpty();
+        if (givenPosition == 1) 
+        {
+          result = firstNode.data;
+          firstNode = firstNode.next;
+        }
+        else 
+        {
+          Node nodeBefore = getNodeAt(givenPosition-1);
+          Node nodeToRemove = nodeBefore.next;
+          Node nodeAfter = nodeToRemove.next;
+          nodeBefore.next = nodeAfter; // disconnect node to be removed
+          result = nodeToRemove.data; // save entry to be removed
+        } // end if
+        length--;
+      } // end if
+      return result;
+    } // end remove
+
+    public boolean contains(OWLIndividual anEntry) 
+    {
+      boolean found = false;
+      Node currentNode = firstNode;
+
+      while (!found && (currentNode != null)) 
+      {
+        if (anEntry.equals(currentNode.data))
+          found = true;
+        else
+          currentNode = currentNode.next;
+      } // end while
+      return found;
+    } // end contains
+
+    public OWLIndividual getEntry(int givenPosition) 
+    {
+      OWLIndividual result = null; // result to return
+
+      if ((givenPosition >= 1) && (givenPosition <= length))
+      {
+          assert !isEmpty();
+          result = getNodeAt(givenPosition).data;
+      }
+      return result;
+    } // end getEntry
+
+        
+    public int getLength() 
+    {
+      return length;
+    }
+
+    public boolean isEmpty() 
+    {
+      boolean result;
+      if(length == 0)
+      {
+        assert firstNode == null;
+        result = true;
+      }
+      else
+      {
+        assert firstNode != null;
+        result = false;
+      } // end if
+    
+      return result;
+
+    }
+
+    public void display() 
+    {
+      Node currentNode = firstNode;
+
+      while (currentNode != null) 
+      {
+        System.out.println("-----"+currentNode.data);
+        currentNode = currentNode.next;
+      }
+
+    }
+
+    public Node getNodeAt(int givenPosition) 
+    {
+      Node currentNode = firstNode;
+
+      // traverse the list to locate the desired node
+      for (int counter = 1; counter < givenPosition; counter++)
+        currentNode = currentNode.next;
+
+      return currentNode;
+    } // end getNodeAt
+
+    private class Node  
+    {
+      private OWLIndividual data; // data portion
+      private Node next; // link to next node
+
+      private Node(OWLIndividual dataPortion)  
+      {
+        data = dataPortion;
+        next = null;
+      } // end constructor
+
+      private Node(OWLIndividual dataPortion, Node nextNode) 
+      {
+        data = dataPortion;
+        next = nextNode;
+      } // end constructor
+
+    } // end Node      
+}
---
+++ branches/beta-rss/src/application/LinkedQueue.java
@@ -0,0 +1,140 @@
+package application;
+
+public class LinkedQueue<T> 
+{
+	private Node firstNode;
+	private Node lastNode;
+	private int size;
+	
+	public LinkedQueue()
+	{
+		firstNode=null;
+		lastNode=null;
+		size=0;
+	}
+
+	public void enqueue(T newEntry)
+	{
+		Node newNode=new Node(newEntry, null);
+		
+		if (isEmpty())
+			firstNode=newNode;
+		else 
+			lastNode.setNextNode(newNode);
+		
+		size++;
+		lastNode=newNode;
+	} //end enqueue
+	
+	public int getSize()
+	{
+		return size;
+	}
+	
+	public T getFront()
+	{
+		T front=null;
+		if (!isEmpty())
+			front=firstNode.getData();
+		
+		return front;
+	}
+	
+	public T dequeue()
+	{
+		T front=null;
+		size--;
+		
+		if (!isEmpty())
+		{
+			front =firstNode.getData();
+			firstNode=firstNode.getNextNode();
+			
+			if (firstNode==null)
+				lastNode=null;
+		}
+		
+		return front;
+	}
+	
+	public String getValues()
+	{
+		Node front=firstNode;
+		String result="";
+		
+		while (front!=null)
+		{
+			result=result+" "+front.getData().toString();
+			front=front.next;
+		}
+		result=result.substring(1,result.length());
+		
+		return result;
+	}
+	
+	
+	public void display()
+	{
+		Node front=firstNode;
+		
+		while (front!=null)
+		{
+			System.out.println(front.getData());
+			front=front.next;
+		}
+			
+	}
+	
+	public boolean isEmpty()
+	{
+		return (firstNode==null) && (lastNode==null);
+	}
+	
+	public void clear()
+	{
+		firstNode=null;
+		lastNode=null;
+	}
+	
+//---------------node class---------------------------
+	private class Node 
+	{
+		private T data;
+		private Node next;
+	
+	
+    private Node(T dataPortion)
+    {
+    	data=dataPortion;
+    	next=null;
+    }
+    
+    private Node(T dataPortion, Node nextNode)
+    {
+    	data=dataPortion;
+    	next=nextNode;
+    }
+    
+    private T getData()
+    {
+    	return data;
+    }
+    
+    private void setData(T newData)
+    {
+    	data=newData;
+    }
+    
+    private Node getNextNode()
+    {
+    return next;	
+    }
+    
+    private void setNextNode(Node nextNode)
+    {
+    	next=nextNode;
+    }
+    
+  }//end node class
+	
+}
